3c5fd38013da826feb19e28444fc7399fd82e1cc
code-JOA <joshbillion1@gmail.com>
2020-06-23 16:31:36 +0100

    scaled numerical columns

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -88,12 +88,13 @@ class Preprocessor:
         self.logger_object.log(self.file_object, 'Entered the is_null_present method of the Preprocessor class')
         self.null_present = False
         self.cols_with_missing_values=[]
+        self.cols = data.columns
         try:
             self.null_counts=data.isna().sum()
             for i in range(len(self.null_counts)):
                 if self.null_counts[i]>0:
                     self.null_present=True
-                    self.cols_with_missing_values.append(slef.cols[i])
+                    self.cols_with_missing_values.append(self.cols[i])
             if(self.null_present): #write the logs to see which columns have null values
                 self.dataframe_with_null = pd.DataFrame()
                 self.dataframe_with_null['columns'] = data.columns
@@ -107,4 +108,39 @@ class Preprocessor:
             raise Exception()
 
 
-    def impute_missing_values(self, data, cols_with_missing_values):        
+    def impute_missing_values(self, data, cols_with_missing_values):
+        """
+           Method Name : impute_missing_values
+           Description : This method replaces all missing values in df using KNN Imputer
+           Output : Returns a DataFrame which has all the missing values imputed
+           On Failure : Raise Exception
+        """
+        self.logger_object.log(self.file_object,"Entered the impute_missing_values method of the Preprocessor class")
+        self.data=data
+        self.cols_with_missing_values=cols_with_missing_values
+        try:
+            self.imputer = CategoricalImputer()
+            for col in self.cols woth missing values:
+                self.data[col] = self.imputer.fit_transform(self.data[col])
+            self.loggoer_object.log(self.file_object, "Imputing missing values Successful. Exited impute_missing_values method of the Preprocessor class")
+            return self.data
+        except Exception as e:
+            self.logger_object.log(self.file_object,"Exception occured in impute_missing_values method of the Preproccesor class. Exception message: " + str(e))
+            self.logger_object.log(self.file_object,"Imputing missing values failed. Exited the impute_missing_values method of the Preproccesor class")
+            raise Exception()
+
+
+    def scale_numerical_columns(self,data):
+        """
+           Method Name : scale_numerical_columns
+           Description : This method scales the numerical columns using the Standard Scaler
+           Output : Returns a DataFrame with scaled values
+           On Failure : Raise Exception
+        """
+        self.logger_object.log(self.file_object, "Entered the scale_numerical_columns method of the Preproccesor class")
+        self.data=data
+        self.num_df = self.data[["months_as_customer","policy_deductable","umbrella_limit",
+                          'capital-gains', 'capital-loss', 'incident_hour_of_the_day',
+                          'number_of_vehicles_involved', 'bodily_injuries', 'witnesses', 'injury_claim',
+                          'property_claim',
+                          'vehicle_claim']]
